19bbb3b038132450a2c3ffeb18a2749f4ff8f32c
Hovixen <orevaerijo@gmail.com>
2023-10-03 22:27:50 +0000

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ for i in range(10):
     for j in range(i + 1, 10):
         if i == 0 and j == 1:
             print("{:02d}".format(1), end=", ")
-        elif j == 9:
+        elif i == 8 and j == 9:
             print("{:02d}".format(i * 10 + j))
         else:
             print("{:02d}".format(i * 10 + j), end=", ")

df1a484b91efa44765dd405cb38a64a6d13ab9a2
Hovixen <orevaerijo@gmail.com>
2023-10-07 13:16:16 +0000

    [FIX]: used an custom enumerate built-in function

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@
 def print_matrix_integer(matrix=[[]]):
     # function prints matrix of integer
     for row in matrix:
-        for col in range(len(row)):
-            print('{:d}'.format(row[col]), end=' ' if col < len(row) - 1 else '')
+        for i in range(len(row)):
+            print('{:d}'.format(row[i]), end=' ' if i < len(row) - 1 else '')
         print()

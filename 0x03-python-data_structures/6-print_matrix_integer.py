45ef22c83ec96f3de22f365ce5f703606a26cff7
Hovixen <orevaerijo@gmail.com>
2023-10-07 12:08:31 +0000

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,8 @@
 def print_matrix_integer(matrix=[[]]):
     # function prints a matrix of integers
     for i in matrix:
-        print(i)
+        for j in i:
+            if j != 0:
+                print(' ', end='')
+            print('{:d}'.format(j), end='')
+        print()

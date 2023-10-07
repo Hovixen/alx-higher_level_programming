85817118ff71d7e5b79d8f94596bea250ea6a14b
Hovixen <orevaerijo@gmail.com>
2023-10-07 07:31:56 +0000

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@
 def element_at(my_list, idx):
     # function retrieves an element at a position in a list
     if idx < 0 or idx >= len(my_list):
-        return 
+        return None
     else:
         return my_list[idx]

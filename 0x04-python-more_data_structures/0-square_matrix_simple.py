bd5b51e43727f1e0fd8c54931f4968a3930a873a
Hovixen <orevaerijo@gmail.com>
2023-10-11 14:50:28 +0000

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,5 +4,5 @@ def square_matrix_simple(matrix=[]):
     # function computes the square value of all integers of a matrix
     # Mrow is the rows in the Matrix
     # e is the elements in the Matrix row
-    matrixNew = list(map(lambda Mrow:list(map(lambda e: e ** 2, Mrow)), matrix))
+    matrixNew = list(map(lambda M.R: list(map(lambda e: e ** 2, M.R)), matrix))
     return matrixNew

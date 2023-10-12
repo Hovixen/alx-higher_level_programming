45601f7accaecf32825ce00fe5b02c790fbfba4d
Hovixen <orevaerijo@gmail.com>
2023-10-12 03:07:25 +0000

    task 11

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,6 @@
 def best_score(a_dictionary):
     # function returns a key with the biggest integer value
     # this can also be simplified using the max() function
-    # or using a comprehension method
     if a_dictionary is None:
         return None
     maxval = float('-inf')

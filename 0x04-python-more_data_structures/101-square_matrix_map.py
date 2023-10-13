db3e72322a280a9f3c2c26a7b887da3922702237
Hovixen <orevaerijo@gmail.com>
2023-10-13 12:07:33 +0000

    task 14

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda R: list(map(lambda e: e ** 2, R)), matrix))
+    return list(map(lambda r: list(map(lambda e: e ** 2, r)), matrix))

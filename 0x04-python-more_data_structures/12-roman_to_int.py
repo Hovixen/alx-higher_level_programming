d2076512d3f64fd76b2b7aa4711ad8bd2230fb7c
Hovixen <orevaerijo@gmail.com>
2023-10-14 19:55:52 +0000

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,23 +2,19 @@
 def roman_to_int(rom):
     if not rom:
         return None
-    rom_dic = {
-            "I": 1, "V": 5,
-            "X": 10, "L": 50,
-            "C": 100, "D": 500,
-            "M": 1000
-            }
+    rom_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
     sums = 0
     prev_num = 0
     for i in rom:
         if i in rom_dic:
             numeral = rom_dic[i]
             if numeral > prev_num:
-                sums += numeral - 2 * prev_num
+                sums += numeral - prev_num
             else:
                 sums += numeral
             prev_num = numeral
         else:
-            print("{} is not a Roman numeral".format(i))
+            # print("{} is not a Roman numeral".format(i))
             return None
-        return sums
+    return sums

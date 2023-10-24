932b36b94bd1bbeb9d2af20b3acb8b4efd957b4b
Hovixen <orevaerijo@gmail.com>
2023-10-24 01:27:54 +0000

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,7 +6,7 @@ def safe_print_division(a, b):
     try:
         ans = a / b
     except ZeroDivisionError:
-        ans None
+        ans = None
     finally:
         print("Inside result: {}".format(ans))
         return ans

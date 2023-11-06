030d5f9b9b5c224615e5776c2026eebcbd8e6dd3
Hovixen <orevaerijo@gmail.com>
2023-11-06 17:04:55 +0000

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+class that inherits from in-built function
+"""
+
+
+class MyList(list):
+    """
+    This class MyList is a child class of the in-built function list
+    and it has a public method that prints the list in sorted order (ascending)
+    """
+
+    def print_sorted(self):
+        """
+        This function sorts the list in ascending order
+        """
+        sortedList = sorted(self)
+        listElements = []
+        for elements in sortedList:
+            listElements.append(elements)
+        print (listElements)

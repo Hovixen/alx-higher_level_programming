5ccb3d5e373bc0a85e831a2938d65d0995265fd7
Hovixen <orevaerijo@gmail.com>
2023-11-06 17:55:46 -0800

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+task 3 function checks if an object is an instance of a class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    function returns True if the object is an instance of the specified class
+    otherwise False
+
+    Args:
+        obj(object): object to be passed and checked
+        a_class (type): class to compare the object to
+
+    Return:
+        bool: True if the object is an instance of the class or
+            False if not                              """
+
+    return isinstance(obj, a_class)

37245ff5ef228942aa32c69cc772ede838e22532
Hovixen <orevaerijo@gmail.com>
2023-11-06 18:00:07 -0800

    task 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,7 +4,7 @@ task 3 function checks if an object is an instance of a class
 """
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """
     function returns True if the object is an instance of the specified class
     otherwise False

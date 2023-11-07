5ea05c23d0ef972a3e8ad6913bd699296c899d3e
Hovixen <orevaerijo@gmail.com>
2023-11-06 18:34:18 -0800

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Base geometry with area not implimented
+"""
+
+
+class BaseGeometry:
+    """ Empty class """
+    def area(self):
+        raise Exception("area() is not implemented")

62fb9c1c6d1b663ef0c97a2712294f067840998c
Hovixen <orevaerijo@gmail.com>
2023-11-07 18:25:11 -0800

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -14,8 +14,5 @@ def number_of_lines(filename=""):
         Exception: when the file can be opened
 
     """
-    line_count = 0
     with open(filename, 'r', encoding="utf-8") as f:
-        for line in f:
-            line_count += 1
-    return line_count
+        return f.write()

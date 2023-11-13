5362db4e36ad2268cd3932a439c2c04f80dd3084
Hovixen <orevaerijo@gmail.com>
2023-11-13 21:08:40 +0000

    task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" This model contains the Base Class """
+
+class Base:
+    """ Base class that manages id attributes in all 
+        future class instances created """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Initializing the Base class """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            # assiging the new value of count to the id
+            self.id = Base.__nb_objects

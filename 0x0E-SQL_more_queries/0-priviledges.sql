46e1ab26b612ded6e96f9cabc9ec0e0c843f81c7
Hovixen <orevaerijo@gmail.com>
2023-12-13 23:36:04 +0000

    task 0

--- 0x0E-SQL_more_queries/0-priviledges.sql
+++ 0x0E-SQL_more_queries/0-priviledges.sql
@@ -1,4 +1,4 @@
--- Show priviledges of user
--- Query displays the priviledges of user_0d_1 and user_0d_2
+-- Script that lists all privileges of the MySQL users
+-- Query to list all privileges (GRANT) of the MySQL users
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';

3b6d2deb202fbb165a113a97228a58e461f749ad
Hovixen <orevaerijo@gmail.com>
2023-12-13 23:28:28 +0000

    task 0

--- 0x0E-SQL_more_queries/0-priviledges.sql
+++ 0x0E-SQL_more_queries/0-priviledges.sql
@@ -1,4 +1,5 @@
 -- Show priviledges of user
 -- Query displays the priviledges of user_0d_1 and user_0d_2
 
-SHOW GRANT user_0d_1, user_0d_2;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

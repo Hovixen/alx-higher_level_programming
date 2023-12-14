b5e59b59093d913973cd296aaace811c5feb253d
Hovixen <orevaerijo@gmail.com>
2023-12-13 17:26:14 -0800

    tasks

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,6 +5,7 @@ USE hbtn_0d_usa;
 
 -- Query to create cities in hbtn_0d    _usa
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-       id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
-       state_id INT FOREIGN KEY REFERENCES states(id) NOT NULL,
-       name VARCHAR(256) NOT NULL);
+	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES states(id));

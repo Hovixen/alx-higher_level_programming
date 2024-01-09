9d679331eb047bd32bb8f158473b8f40752fbfcb
Hovixen <orevaerijo@gmail.com>
2024-01-09 02:51:04 +0000

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,8 @@
 const [,, arg] = process.argv;
 
 if (!isNaN(parseInt(arg))) {
-  console.log('My number: ', parseInt(arg));
+  console.log('My number: ' + parseInt(arg));
 } else {
   console.log('Not a number');
 }
+

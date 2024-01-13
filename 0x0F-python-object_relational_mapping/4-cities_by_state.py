2e86e44882e3bdb931b833bba115ddc2c934e3cd
Hovixen <orevaerijo@gmail.com>
2024-01-13 09:59:24 +0000

    task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 
     connX = MySQLdb.connect(host="localhost", port=3306, user=user,
                             passwd=password, db=db)
-    cur= connX.cursor()
+    cur = connX.cursor()
 
     sql_query = (
             "SELECT cities.id, cities.name, states.name "

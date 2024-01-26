28cac5bf10029a68e67fdcde0181148e39d1c5a1
Hovixen <orevaerijo@gmail.com>
2024-01-26 21:31:49 +0000

    task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+
+if __name__ == '__main__':
+    import requests
+
+    url = "https://alx-intranet.hbtn.io/status"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        body = response.text
+        print("Body Response:")
+        print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))

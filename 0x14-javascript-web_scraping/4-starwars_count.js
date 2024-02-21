ebbfb28a74dff2ae36e8178429f633204646c77f
Hovixen <orevaerijo@gmail.com>
2024-02-21 00:58:15 +0000

    task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const [,, arg] = process.argv;
+
+const characterId = 18;
+
+request(arg, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+  const film = JSON.parse(body);
+
+  let filmCount = 0;
+
+  film.results.forEach((film) => {
+    if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+      filmCount++;
+    }
+  });
+  console.log(filmCount);
+});
